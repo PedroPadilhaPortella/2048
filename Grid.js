47f99a3f47de035a7d6d888155cbdc66b27b78e7
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-03-10 12:56:26 -0300

    handling keyboard inputs

--- Grid.js
+++ Grid.js
@@ -30,6 +30,22 @@ export default class Grid {
         });
     }
 
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || [];
+            cellGrid[cell.x][cell.y] = cell;
+            return cellGrid;
+        }, [])
+    }
+
+    get cellsByRow() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.y] = cellGrid[cell.y] || [];
+            cellGrid[cell.y][cell.x] = cell;
+            return cellGrid;
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null);
     }
@@ -45,6 +61,7 @@ class Cell {
     #x
     #y
     #tile
+    #mergeTile
 
     constructor(cellElement, x, y) {
         this.#cellElement = cellElement;
@@ -52,8 +69,27 @@ class Cell {
         this.#y = y;
     }
 
+    get x() {
+        return this.#x
+    }
+
+    get y() {
+        return this.#y
+    }
+
     get tile() {
         return this.#tile
+    }
+
+    get mergeTile() {
+        return this.#mergeTile
+    }
+
+    set mergeTile(value) {
+        this.#mergeTile = value;
+        if(value == null) return;
+        this.#mergeTile.x = this.#x;
+        this.#mergeTile.y = this.#y;
     }
 
     set tile(value) {
@@ -62,5 +98,9 @@ class Cell {
 
         this.#tile.x = this.#x;
         this.#tile.y = this.#y;
+    }
+
+    canAccept(tile) {
+        return (this.tile == null || (this.mergeTile == null && this.tile.value == tile.value));
     }
 }
